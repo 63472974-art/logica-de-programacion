9f9ab61941a0a0b2903ad0ffee54729d9fade093
63472974-art <63472974@institutourusayhua.edu.pe>
2026-09-25 12:04:07 -0500

    subir

--- operativos_cadena.py
+++ operativos_cadena.py
@@ -1,9 +1,9 @@
-
+#cadena
 # print("cadena 1"+"cadena 2")
 # nom1= "rodrigo"
 # nom2= "yhon"
 # print(nom1 + " y " + nom2 + ": son compañeros")
-
+#recopilacion
 # nombre = input("¿Me puedes dar tu nombre por favor? ")
 # apellido = input("¿Me puedes dar tu apellido por favor? ")
 # print("Gracias. ")
@@ -13,3 +13,6 @@ print("james" * 3)
 print(3*"an")
 print("2" * 5)
 
+print("+" + 30 * "❤️" + "+")
+print(("|" + " " * 35 + "💕\n") * 5, end="")
+print("+" + 30 * "❤️" + "+")
